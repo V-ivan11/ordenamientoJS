300744b64ce8fdb391f1bf1db250b1e3113c9065
Ivan V <ivanverduzco11@gmail.com>
2022-03-14 20:00:44 -0600

    Versión FInal
    Co-authored-by: Juan Pablo Suárez Pérez <breko151@users.noreply.github.com>

--- src/funciones.js
+++ src/funciones.js
@@ -1,21 +1,46 @@
-function insertionSort(D){
-    for (var i = 1; i < D.length; i++) {
-        var j = i;
-        while(j > 0 && D[j] < D[j-1]){
-            var aux = D[j];
-            D[j] = D[j-1];
-            D[j-1] = aux;
-            j = j-1;
-        }
+function recogerDatos() {
+    var n = 8;
+    var datos = new Array(n);
+    for (let i = 0; i < n; i++) {
+        datos[i] = parseInt(document.getElementById(i + 1).value);
+
     }
-    return D;
+    return datos;
 }
 
-function selectionSort(D){
+function colocarResultado(D) {
+    var identificadores = ["r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8"];
+
+    for (let i = 0; i < D.length; i++) {
+        document.getElementById(identificadores[i]).innerHTML = String(D[i]);
+    }
+}
+
+function insertionSort() {
+
+    D = recogerDatos();
+
+    for (var i = 1; i < D.length; i++) {
+        var j = i;
+        while (j > 0 && D[j] < D[j - 1]) {
+            var aux = D[j];
+            D[j] = D[j - 1];
+            D[j - 1] = aux;
+            j = j - 1;
+        }
+    }
+
+    colocarResultado(D);
+}
+
+function selectionSort() {
+
+    D = recogerDatos();
+
     for (var i = 0; i < D.length; i++) {
         var min = i;
-        for(var j = i; j < D.length; j++){
-            if(D[min] > D[j]){
+        for (var j = i; j < D.length; j++) {
+            if (D[min] > D[j]) {
                 min = j;
             }
         }
@@ -23,18 +48,24 @@ function selectionSort(D){
         D[min] = D[i];
         D[i] = aux;
     }
-    return D;
+
+    colocarResultado(D);
 }
 
-function bubbleSort(D) {
-    for (var i = 0; i < D.length-1; i++) {
-        for(var j = 0; j < (D.length-i-1); j++){
-            if(D[j]>D[j+1]){
-                var aux = D[j];
-                D[j] = D[j+1];
-                D[j+1] = aux;
+function bubbleSort() {
+
+    D = recogerDatos();
+    
+    for (var i = 0; i < D.length - 1; i++) {
+        for (var j = 0; j < D.length - i - 1; j++) {
+            if (D[j] > D[j + 1]) {
+                var aux = D[j]
+                D[j] = D[j + 1]
+                D[j + 1] = aux
             }
+
         }
     }
-    return D;
+
+    colocarResultado(D);
 }
